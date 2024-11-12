4f43446a0dead00017a5ad4f22dd02b72311276c
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-11-12 11:59:33 +0100

    Base layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-    <AppNav />
-    <slot />
-    <AppFooter />
+    <AppNav class="fixed h-screen w-24 bg-blue-500" />
+    <div class="ml-24">
+        <main class="bg-red-500">
+            <slot />
+        </main>
+        <AppFooter class="bg-yellow-500" />
+    </div>
 </template>
