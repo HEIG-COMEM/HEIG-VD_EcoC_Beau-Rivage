7f1851241ead1e9a907c5b6589dba1eab6ee997d
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-11-12 11:18:50 +0100

    prettier

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+    <AppNav />
+    <slot />
+    <AppFooter />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="bg-red-600 font-mono">
+        tailwindcss: { cssPath: '~/assets/css/tailwind.css', }
+    </div>
+</template>
+
+<style scoped></style>
