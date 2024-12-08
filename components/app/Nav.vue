78e030568414bc8f07d7c5d62cfbb52547982dca
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-12-08 15:20:56 +0100

    feat(navbar): first version

--- components/app/Nav.vue
+++ components/app/Nav.vue
@@ -1,7 +1,137 @@
-<script setup></script>
+<script setup>
+const isMenuOpen = ref(false);
+</script>
 
 <template>
-    <nav>Nav</nav>
+    <header class="fixed">
+        <div
+            class="left-column absolute z-20 flex flex-col items-center justify-between bg-backgroundSand py-4"
+        >
+            <div class="menu-icon" @click="isMenuOpen = !isMenuOpen">
+                <SvgBurgerMenu />
+                <span class="text-sm font-bold uppercase">Menu</span>
+            </div>
+            <div>
+                <a href="#" class="py-5">
+                    <SvgGift />
+                </a>
+            </div>
+        </div>
+        <nav
+            class="absolute flex h-full flex-col justify-between bg-white text-xl"
+            :class="{ showMenu: !isMenuOpen }"
+        >
+            <div>
+                <form action="" method="get" class="mb-8">
+                    <div class="flex flex-row">
+                        <input
+                            type="search"
+                            name="search"
+                            id="search"
+                            placeholder="Rechercher"
+                            class=""
+                        />
+                        🔍
+                    </div>
+                </form>
+                <ul>
+                    <li>
+                        <router-link to="/">Hôtel</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/rooms-suites"
+                            >Chambres & Suites</router-link
+                        >
+                    </li>
+                    <li>
+                        <router-link to="/restaurants-bars"
+                            >Restaurants & Bars</router-link
+                        >
+                    </li>
+                    <li>
+                        <router-link to="/spa">Spa Guerlain</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/meeting-event"
+                            >Réunions & Évenements</router-link
+                        >
+                    </li>
+                    <li>
+                        <router-link to="/leaders-club"
+                            >Leaders Club</router-link
+                        >
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <div class="socials flex flex-row items-center gap-4">
+                    <a rel="noreferrer nofollow" aria-label="Facebook" href="#"
+                        ><SvgFacebookLogo />
+                    </a>
+                    <a rel="noreferrer nofollow" aria-label="Instagram" href="#"
+                        ><SvgInstagramLogo />
+                    </a>
+                    <a rel="noreferrer nofollow" aria-label="LinkedIn" href="#"
+                        ><SvgLinkedinLogo
+                    /></a>
+                    <a rel="noreferrer nofollow" aria-label="Youtube" href="#"
+                        ><SvgYoutubeLogo />
+                    </a>
+                </div>
+                <div class="newsletter mt-5">
+                    <a class="py-5 text-xl" href="#"
+                        >S’inscrire à la newsletter</a
+                    >
+                </div>
+                <div class="leaders-club mt-8">
+                    <a
+                        class="flex w-fit flex-row items-center gap-2 bg-backgroundSand px-8 py-5 text-sm font-bold uppercase"
+                        href="#"
+                    >
+                        <SvgLeadersClubSigle /> Leaders club</a
+                    >
+                </div>
+            </div>
+        </nav>
+    </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+    top: 0;
+    left: 6rem;
+    width: 50vw;
+    padding: 8vh 5vw;
+    background-color: white;
+    transform: translateX(-150%);
+    transition: transform 0.3s ease-in-out;
+    z-index: 999;
+}
+
+nav.showMenu {
+    transform: translateX(0);
+}
+
+nav ul li:not(:last-child) {
+    margin-bottom: 1.5rem;
+}
+
+.left-column {
+    padding-top: 8vh;
+    padding-bottom: 8vh;
+    /* height: calc(100vh - 16vh); */
+    height: 100%;
+    width: 6rem;
+    z-index: 9999;
+    top: 0;
+    left: 0;
+}
+
+.menu-icon {
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 0.5rem;
+}
+</style>
