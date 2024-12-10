4f01eb06dff325fe8a160eb5b9d613c9ac753505
Jeremie Zurfluh <zurfluh.jeremie@gmail.com>
2024-12-10 14:04:25 +0100

    feat(eventCard): refactor EventCard component layout and remove unused imports

--- components/base/EventCard.vue
+++ components/base/EventCard.vue
@@ -1,6 +1,4 @@
 <script setup>
-import IconPlus from '../icons/IconPlus.vue';
-
 const props = defineProps({
     date: {
         type: String,
@@ -25,40 +23,38 @@ const date = new Date(props.date);
 const options = { year: 'numeric', month: 'short', day: 'numeric' };
 const formattedDate = date.toLocaleDateString('en-GB', options);
 const [day, month, year] = formattedDate.split(' ');
-
-
-
 </script>
 
 <template>
     <div class="max-w-sm overflow-hidden bg-white">
         <div class="flex p-6">
-            <div class="rounded-full h-32 w-32 bottom-4 left-4 bg-backgroundAccent flex items-center justify-center">
+            <div
+                class="bg-backgroundAccent bottom-4 left-4 flex h-32 w-32 items-center justify-center rounded-full"
+            >
                 <div class="text-center">
-                    <p class="
-                        text-2xl
-                        font-bold">{{ day }}</p>
-                    <p class="text-xs
-                        uppercase">{{ month }}</p>
-                    <p class="text-xs
-                        uppercase">{{ year }}</p>
+                    <p class="text-2xl font-bold">{{ day }}</p>
+                    <p class="text-xs uppercase">{{ month }}</p>
+                    <p class="text-xs uppercase">{{ year }}</p>
                 </div>
             </div>
         </div>
         <div class="relative px-6 pb-16">
-            <div class="flex flex-col w-64">
-                <div class="font-bold text-xl mb-2 w-64 uppercase">{{ title }}</div>
+            <div class="flex w-64 flex-col">
+                <div class="mb-2 w-64 text-xl font-bold uppercase">
+                    {{ title }}
+                </div>
                 <div class="text-lg font-thin">{{ desc }}</div>
             </div>
-                <a :href="href" class="absolute bottom-0 right-0 bg-backgroundAccent h-16 w-16 flex items-center justify-center">
-                    <div class="indicator-item">
-                        <IconPlus class="" />
-                    </div>
-                </a>
+            <a
+                :href="href"
+                class="bg-backgroundAccent absolute bottom-0 right-0 flex h-16 w-16 items-center justify-center"
+            >
+                <div class="indicator-item">
+                    <SvgIconPlus class="" />
+                </div>
+            </a>
         </div>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
